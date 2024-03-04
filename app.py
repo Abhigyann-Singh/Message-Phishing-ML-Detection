e39b7be44f15c66d5e4da0229bfc2d06b1e54809
Abhigyann-Singh <singhabhigyan25@gmail.com>
2024-03-05 04:47:45 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask ,render_template, request
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+vectorizer = pickle.load(open('Mlmodels/tfidf.pkl', 'rb'))
+model = pickle.load(open('Mlmodels/model.pkl', 'rb'))
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/message')
+def products():
+    return render_template('messages.html')
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    if request.method == 'POST':
+        # print(request.form)
+        message = request.form['message']
+        # print(message)
+        message = np.array([message])
+        print(message)
+        ttransformed = vectorizer.transform(message)
+        my_prediction = model.predict(ttransformed)
+    if my_prediction[0] == 0:
+        return render_template('messages.html',msggg= message[0], prediction='Not Spam')
+    else:
+        return render_template('messages.html',msggg= message[0], prediction='Spam')
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
